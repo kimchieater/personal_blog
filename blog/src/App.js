aac095136e7e1f0c481e08bd8e33757f528fc113
kimchieater <jasonj19276@gmail.com>
2024-02-01 16:48:04 +1100

    learned about dynamic UI screen

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,6 +6,13 @@ function App() {
 
   let [title, setTitle] = useState(['남자 코트 추천','강남 우동 맛집',  '파이썬 독학']);
   let [number, setNumber] = useState(0)
+  let [modal, setModal] = useState("false")
+
+
+
+  let [likes, setLikes] = useState([0,0,0])
+
+
 
   return (
     <div className="App">
@@ -14,37 +21,52 @@ function App() {
         <h4 >Dev Blog</h4>
       </div>
 
-      <div className="list">
-
+      {/* <div className="list">
         <button onClick={()=>{
           let copy = [...title];
           copy.sort();
           setTitle(copy);
         }}>가나다 순 정렬</button>
-        <h4>{title[0]}<span onClick={()=>{
+        <h4 onClick={()=>{
+          setModal(!modal);
+        }}>{title[0]}<span onClick={()=>{
           setNumber(number+1)
         }}>좋아요</span> {number}</h4>
         <p>2월 17일</p>
       </div>
       
       <div className="list">
-        <h4>{title[1]}</h4>
-
+        <h4 onClick={()=>{
+          setModal(true)
+        }}>{title[1]}</h4>
         <button onClick={()=>{
           let temp = [...title];
           temp[1] = "여자 코트 추천";
           setTitle(temp);
         }}>Click me</button>
-
         <p>2월 17일</p>
-      </div>
+      </div> */}
 
-      <div className="list">
-        <h4>{title[2]}</h4>
-        <p>2월 17일</p>
-      </div>
       
-      <Modal></Modal>
+      {
+        title.map((a, i)=>{
+          return (
+          <div className="list">
+            <h4 onClick={()=>{
+              setModal(!modal);
+              }}>{title[i]}<span onClick={()=>{
+              let copy = [...likes];
+              copy[i] = copy[i] + 1;
+              setLikes(copy);
+            }}>Like</span>{likes[i]}</h4>
+            <p>2월 17일</p>
+          </div>
+        )
+        })
+      }
+      {
+        modal === true ? <Modal></Modal> : null
+      }
 
     
     </div>
